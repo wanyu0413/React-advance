edf84199926927b2b10af4c8f70f5e69003a7726
Wanyu Jiang <wanyu19930930@gmail.com>
2023-07-27 23:49:26 +0900

    add the login and logout logic

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
   // for better IDE auto-completion
+  onLogin: (email, password) => {},
 });
 
 export const AuthCOntextProvider = (props) => {
-  return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
