87afc552907f5bce271feb7df4f08594b955b77b
Wanyu Jiang <wanyu19930930@gmail.com>
2023-07-27 23:35:52 +0900

    logout dummy function

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,6 +2,8 @@ import React from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  // for better IDE auto-completion
 });
 
 export default AuthContext;
