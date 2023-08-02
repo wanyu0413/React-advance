bf5a06f65d882ccb601687eeef2f92601f6eb14f
Wanyu Jiang <wanyu19930930@gmail.com>
2023-08-02 22:47:00 +0900

    React.forwardRef

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -94,7 +94,9 @@ const Login = (props) => {
     if (formIsValid) {
       authCtx.onLogin(emailState.value, passwordState.value);
     } else if (!emailIsValid) {
+      emailInputRef.current.focus();
     } else {
+      passwordInputRef.current.focus();
     }
   };
 
