2503c862b3d8a27da7ddb28daee0a7697467e00b
Wanyu Jiang <wanyu19930930@gmail.com>
2023-08-02 22:07:37 +0900

    current.focus()

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -82,7 +82,11 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+    } else {
+    }
   };
 
   return (
@@ -107,7 +111,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./Input.module.css";
 
 const Input = (props) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
   return (
     <div
       className={`${classes.control} ${
@@ -10,6 +16,7 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
